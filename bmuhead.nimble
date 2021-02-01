e9303c2cdf9fab7c5e65da4070e36cef68960b25
Hendrik Albers <mail@hendrik.dev>
2021-02-01 19:59:41 +0100

    add bmu header to files

---
+++ bmuhead.nimble
@@ -0,0 +1,13 @@
+# Package
+
+version       = "0.1.0"
+author        = "Hendrik Albers"
+description   = "Add BMU header to files"
+license       = "MIT"
+srcDir        = "src"
+bin           = @["bmuhead"]
+
+
+# Dependencies
+
+requires "nim >= 1.4.2"
---
+++ src/bmuhead.nim
@@ -0,0 +1,42 @@
+import os, sequtils, strformat
+
+const
+  bmuHeader = "BMU V1.0"
+  fileExtensions = [".bmu", ".wav"]
+  usage = &"""
+Provide one or more filenames as parameters.
+The text "{bmuHeader}" will be added to the beginning of each file.
+If the file already starts like that nothing will be done.
+"""
+
+if paramCount() == 0 or commandLineParams().anyIt it in ["-h", "--h", "-help", "--help"]:
+  echo usage
+  quit(QuitSuccess)
+
+for fn in commandLineParams():
+  stdout.write(fn & ": ")
+  stdout.flushFile
+  if not fn.fileExists:
+    echo "file not found"
+    continue
+  if fn.splitFile.ext notin fileExtensions:
+    echo "skipping because extension does not match " & $fileExtensions
+    continue
+  let srcFile = open(fn, fmRead)
+  var fileHead = newString(bmuHeader.len)
+  if srcFile.readChars(fileHead, 0, bmuHeader.len) == bmuHeader.len and fileHead == bmuHeader:
+    echo &"\"{bmuHeader}\" header already present"
+  else:
+    srcFile.setFilePos(0)
+    let outFn = ".temp-" & fn & ".bmuhead"
+    let outFile = open(outFn, fmWrite)
+    outFile.write(bmuHeader)
+    var buffer: array[4096, byte]
+    while true:
+      let bytesRead = srcFile.readBytes(buffer, 0, buffer.len)
+      if bytesRead == 0: break
+      doAssert outFile.writeBytes(buffer, 0, bytesRead) == bytesRead, "Error writing to temporary file"
+    outFile.close
+    moveFile(outFn, fn)
+    echo &"\"{bmuHeader}\" header added"
+  srcFile.close
